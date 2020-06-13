1e3e92e1bb529a1efb6c5453d50558575205c6a8
venkat <venkatavaradan.r@gmail.com>
2020-06-13 18:58:54 +0530

    this is a pain

--- app.py
+++ app.py
@@ -12,26 +12,15 @@ app.secret_key = b'\xd2(*K\xa0\xa8\x13]g\x1e9\x88\x10\xb0\xe0\xcc'
 app.config["DEBUG"] = True
 
 # Loads the Database and Collections
-# mongo = pymongo.MongoClient(
-#     'mongodb+srv://admin:admin@cluster0-gjuwr.gcp.mongodb.net/safedb?retryWrites=true&w=majority', maxPoolSize=50, connect=True)
-# db = pymongo.database.Database(mongo, 'safedb')
-
-# client = pymongo.MongoClient(
-#     "mongodb+srv://admin:admincluster0-gjuwr.gcp.mongodb.net/safedb?retryWrites=true&w=majority")
-# db = client.safedb
-
-# mongo = pymongo.MongoClient(
-#     'mongodb+srv://srujandeshpande:mongodb@cluster0-e0fen.azure.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=True)
-# db = pymongo.database.Database(mongo, 'covid_v1')
 
 client = pymongo.MongoClient(
     "mongodb+srv://admin:admin@cluster0-gjuwr.gcp.mongodb.net/safedb?retryWrites=true&w=majority")
 db = client.safedb
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "sup"
 
 
 @app.route('/test')
